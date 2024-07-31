9956f23b33d326687f58bbd6f5c730e1903f4b32
Oleg <antoshinov@gmail.com>
2024-07-31 20:36:29 +1000

    mistake

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1','webserver', python-project-52-xjo5.onrender.com]
+ALLOWED_HOSTS = ['127.0.0.1','webserver', 'python-project-52-xjo5.onrender.com']
 
 
 # Application definition
