8a9ce85ac7256803c93a2e1857c1fe5e2a8857e8
Oleg <antoshinov@gmail.com>
2024-08-03 20:35:17 +1000

    adding new language

--- main/views.py
+++ main/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+from django.utils import translation
 
 
-def index(request):
-    return render(request, 'main/index.html')
+class IndexView(View):
 
+    def get(self, request):
+        if request.LANGUAGE_CODE == 'ru':
+            translation.activate(request.LANGUAGE_CODE)
+        print(f'Current language code is {request.LANGUAGE_CODE}')
+        return render(request, 'main/index.html')
