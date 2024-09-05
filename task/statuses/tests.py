2a4b7fcf7d3cd5ac5a981b7dde7dd4859304c170
Oleg <antoshinov@gmail.com>
2024-09-05 21:10:50 +1000

    adding statuses

--- task/statuses/tests.py
+++ task/statuses/tests.py
@@ -2,52 +2,51 @@ from django.test import TestCase
 from django.test import Client
 from django.urls import reverse
 from .models import Status
+from task.users.models import User
 
 
-# class TestCRUD(TestCase):
+class TestCRUD(TestCase):
 
-# 	def setUp(self):
-# 		self.client = Client()
-# 		Status.objects.create('first_status')
+	def setUp(self):
+		self.client = Client()
+		current_status = {'name': 'first_status'}
+		user_data = {'username': 'testuser',
+					 'password': 'secret'}
+		User.objects.create_user(**user_data)
+		self.client.post(reverse('login'), user_data)
+		Status.objects.create(**current_status)
 
 
-# 	def test_create_status_GET(self):
-# 		response = self.client.get(reverse('create_status'))
+
+	def test_create_status_GET(self):
+		response = self.client.get(reverse('create_status'))
 		
-# 		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.status_code, 200)
 
 
-# 	def test_update_GET(self):
-# 		response = self.client.get(reverse('update', args=[1, ]))
+	def test_update_GET(self):
+		response = self.client.get(reverse('update_status', args=[1, ]))
 		
-# 		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.status_code, 200)
 
 
-# 	def test_delete_GET(self):
-# 		response = self.client.get(reverse('delete', args=[1, ]))
+	def test_delete_GET(self):
+		response = self.client.get(reverse('delete_status', args=[1, ]))
 		
-# 		self.assertEqual(response.status_code, 200)
-		
-	
-# 	def test_create_POST(self):
-# 		self.client.post(reverse('register'), {'first_name': 'Kirill',
-# 														  'last_name': 'Ivanov',
-# 														  'username': 'Kirril',
-# 														  'password1': '123',
-# 														  'password2': '123'})
-		
-# 		self.assertEqual(len(User.objects.all()), 2)
+		self.assertEqual(response.status_code, 200)
 		
 	
-# 	def test_delete_POST(self):
-# 		self.client.post(reverse('delete', args=[1, ]))
+	def test_create_POST(self):
+		self.client.post(reverse('create_status'), {'name': 'second_status'})
 		
-# 		self.assertEqual(len(User.objects.all()), 0)
+		self.assertEqual(len(Status.objects.all()), 2)
 		
-# 	def test_update_POST(self):
-# 		self.client.post(reverse('update', args=[1, ]), {'first_name': 'New',
-# 														  'last_name': 'User',
-# 														  'username': 'Gena',
-# 														  'password1': '123',
-# 														  'password2': '123'})
-# 		self.assertEqual(User.objects.filter(pk=1).values()[0]['username'], 'Gena')
+	
+	def test_delete_POST(self):
+		self.client.post(reverse('delete_status', args=[1, ]))
+		
+		self.assertEqual(len(Status.objects.all()), 0)
+		
+	def test_update_POST(self):
+		self.client.post(reverse('update_status', args=[1, ]), {'name': 'changed_status'})
+		self.assertEqual(Status.objects.filter(pk=1).values()[0]['name'], 'changed_status')
