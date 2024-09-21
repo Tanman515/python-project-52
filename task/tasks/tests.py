709c23ac293299f47c7e188521b1ab18e99c5345
Oleg <antoshinov@gmail.com>
2024-09-21 14:36:53 +1000

    adding tests

--- task/tasks/tests.py
+++ task/tasks/tests.py
@@ -103,31 +103,27 @@ class TaskTests(TestCase):
 class TaskFilterTests(TestCase):
     def setUp(self):
         self.user1 = get_user_model().objects.create_user(
-            username='user1',
-            password='password123'
+            username='testuser1',
+            password='password'
         )
         self.user2 = get_user_model().objects.create_user(
-            username='user2',
-            password='password123'
+            username='testuser2',
+            password='password'
         )
-        self.client.login(username='user1', password='password123')
+        self.client.login(username='testuser1', password='password')
 
         self.status1 = Status.objects.create(
-            name='Open',
-            description='Open tasks'
+            name='In development',
         )
         self.status2 = Status.objects.create(
-            name='Closed',
-            description='Closed tasks'
+            name='Finished',
         )
 
-        self.labels1 = Label.objects.create(
-            name='Bug',
-            description='Bug related task'
+        self.label1 = Mark.objects.create(
+            name='Bug'
         )
-        self.labels2 = Label.objects.create(
-            name='Feature',
-            description='Feature related task'
+        self.label2 = Mark.objects.create(
+            name='Feature'
         )
 
         self.task1 = Task.objects.create(
@@ -137,7 +133,7 @@ class TaskFilterTests(TestCase):
             executor=self.user1,
             author=self.user1
         )
-        self.task1.labels.add(self.labels1)
+        self.task1.label.add(self.label1)
 
         self.task2 = Task.objects.create(
             name='Task 2',
@@ -146,7 +142,7 @@ class TaskFilterTests(TestCase):
             executor=self.user2,
             author=self.user2
         )
-        self.task2.labels.add(self.labels2)
+        self.task2.label.add(self.label2)
 
     def test_filter_by_status(self):
         url = reverse('tasks')
@@ -164,7 +160,7 @@ class TaskFilterTests(TestCase):
 
     def test_filter_by_labels(self):
         url = reverse('tasks')
-        response = self.client.get(url, {'labels': self.labels1.id})
+        response = self.client.get(url, {'label': self.label1.id})
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Task 1')
         self.assertNotContains(response, 'Task 2')
@@ -181,7 +177,7 @@ class TaskFilterTests(TestCase):
         response = self.client.get(url, {
             'status': self.status1.id,
             'executor': self.user1.id,
-            'labels': self.labels1.id,
+            'label': self.label1.id,
             'self_tasks': 'on'
         })
         self.assertEqual(response.status_code, 200)
