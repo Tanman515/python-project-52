bd8d0cd6592ff5cc7924407e2d7fa6c3117aa770
Oleg <antoshinov@gmail.com>
2024-09-26 11:10:37 +1000

    add test

--- task/users/tests.py
+++ task/users/tests.py
@@ -3,60 +3,32 @@ from django.urls import reverse
 from django.contrib.auth import get_user_model
 
 
-class CreateUserTest(TestCase):
-    def test_create_user_view(self):
-        url = reverse('user_create')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'users/create.html')
-
-    def test_create_user(self):
-        url = reverse('user_create')
-        user_data = {
-            'username': 'testuser',
-            'password1': 'Testpassword123',
-            'password2': 'Testpassword123',
-            'first_name': 'Test',
-            'last_name': 'User'
-        }
-        response = self.client.post(url, user_data)
-        if response.status_code != 302:
-            print(response.content)
-        self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, reverse('login'))
-        self.assertTrue(get_user_model().objects.filter(
-            username='testuser'
-        ).exists())
-
-
-class UpdateUserTest(TestCase):
+class UserTest(TestCase):
     def setUp(self):
         self.user = get_user_model().objects.create_user(
             username='testuser',
-            password='Testpassword123',
+            password='Testpassword',
             first_name='Test',
             last_name='User'
         )
-        self.client.login(username='testuser', password='Testpassword123')
+        self.client.login(username='testuser', password='Testpassword')
 
-    def test_update_user_view(self):
-        url = reverse('user_update', args=[self.user.pk])
+    def test_update_user_get(self):
+        url = reverse('update', args=[self.user.pk])
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'users/update.html')
 
-    def test_update_user(self):
-        url = reverse('user_update', args=[self.user.pk])
+    def test_update_user_post(self):
+        url = reverse('update', args=[self.user.pk])
         updated_data = {
             'username': 'updateduser',
-            'password1': 'Newpassword123',
-            'password2': 'Newpassword123',
+            'password1': 'Newpassword',
+            'password2': 'Newpassword',
             'first_name': 'Updated',
             'last_name': 'User'
         }
         response = self.client.post(url, updated_data)
-        if response.status_code != 302:
-            print(response.content)
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('users'))
         self.user.refresh_from_db()
@@ -64,23 +36,14 @@ class UpdateUserTest(TestCase):
         self.assertEqual(self.user.first_name, 'Updated')
         self.assertEqual(self.user.last_name, 'User')
 
-
-class DeleteUserTest(TestCase):
-    def setUp(self):
-        self.user = get_user_model().objects.create_user(
-            username='testuser',
-            password='Testpassword123'
-        )
-        self.client.login(username='testuser', password='Testpassword123')
-
-    def test_delete_user_view(self):
-        url = reverse('user_delete', args=[self.user.pk])
+    def test_delete_user_get(self):
+        url = reverse('delete', args=[self.user.pk])
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'users/delete.html')
 
-    def test_delete_user(self):
-        url = reverse('user_delete', args=[self.user.pk])
+    def test_delete_user_post(self):
+        url = reverse('delete', args=[self.user.pk])
         response = self.client.post(url)
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('users'))
@@ -88,18 +51,33 @@ class DeleteUserTest(TestCase):
             username='testuser'
         ).exists())
 
-
-class UserListViewTest(TestCase):
-    def setUp(self):
-        self.user = get_user_model().objects.create_user(
-            username='testuser',
-            password='Testpassword123'
-        )
-        self.client.login(username='testuser', password='Testpassword123')
-
     def test_user_list_view(self):
         url = reverse('users')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'users/index.html')
+        self.assertTemplateUsed(response, 'users/list.html')
         self.assertContains(response, 'testuser')
+
+
+class CreateUserTest(TestCase):
+    def test_create_user_get(self):
+        url = reverse('register')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/register.html')
+
+    def test_create_user(self):
+        url = reverse('register')
+        user_data = {
+            'username': 'testuser',
+            'password1': 'Testpassword',
+            'password2': 'Testpassword',
+            'first_name': 'Test',
+            'last_name': 'User'
+        }
+        response = self.client.post(url, user_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('login'))
+        self.assertTrue(get_user_model().objects.filter(
+            username='testuser'
+        ).exists())
