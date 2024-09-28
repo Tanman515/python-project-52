3dc123418757e09e04f1113a1fb9669b4b67f533
Oleg <antoshinov@gmail.com>
2024-09-28 15:29:10 +1000

    refactoring

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -9,7 +9,7 @@ class Task(models.Model):
 	description = models.TextField()
 	status = models.ForeignKey('statuses.Status', on_delete=models.PROTECT)
 	author = models.ForeignKey('users.User', on_delete=models.PROTECT, related_name='created_by', editable=False)
-	executor = models.ForeignKey('users.User', on_delete=models.PROTECT, null=True, related_name='task_for')
+	executor = models.ForeignKey('users.User', on_delete=models.PROTECT, null=True, blank=True, related_name='task_for')
 	label = models.ManyToManyField('marks.Mark', related_name='task_label', blank=True)
 	created_at = models.DateTimeField(auto_now_add=True)
 	
