a89e58270643fbc574c5772ebcbb97e6afff2746
Oleg <antoshinov@gmail.com>
2024-10-02 11:24:04 +1000

    refactoring

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.utils.translation import gettext as _
+from task_manager.statuses.models import Status
+from task_manager.users.models import User
+from task_manager.marks.models import Mark
 
 
 
@@ -7,10 +10,10 @@ class Task(models.Model):
 
 	name = models.CharField(max_length=255, verbose_name=_('Name'))
 	description = models.TextField(blank=True, null=True)
-	status = models.ForeignKey('statuses.Status', on_delete=models.PROTECT, null=False, blank=False)
-	author = models.ForeignKey('users.User', on_delete=models.PROTECT, related_name='created_by', editable=False, null=False, blank=False)
-	executor = models.ForeignKey('users.User', on_delete=models.PROTECT, null=True, blank=True, related_name='task_for')
-	label = models.ManyToManyField('marks.Mark', related_name='task_label', blank=True)
+	status = models.ForeignKey(Status, on_delete=models.PROTECT, null=False, blank=False)
+	author = models.ForeignKey(User, on_delete=models.PROTECT, related_name='created_by', editable=False, null=False, blank=False)
+	executor = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True, related_name='task_for')
+	label = models.ManyToManyField(Mark, related_name='task_label', blank=True)
 	created_at = models.DateTimeField(auto_now_add=True)
 	
 	def __str__(self):
--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -4,3 +4,6 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
 	class Meta:
 		ordering = ['id']
+
+	def __str__(self):
+		return self.get_full_name()
